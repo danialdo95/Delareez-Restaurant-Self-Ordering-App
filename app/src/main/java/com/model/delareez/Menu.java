8e34a89609cf1a12433e4e26283b8613ff3c0d03
danialdo95 <danialdo95@gmail.com>
2017-09-30 23:17:50 +0800

    initial commit

--- app/src/main/java/com/model/delareez/Menu.java
+++ app/src/main/java/com/model/delareez/Menu.java
@@ -1,8 +1,19 @@
 package com.model.delareez;
 
+import android.media.Image;
+
+
 /**
  * Created by User on 26/9/2017.
  */
 
 public class Menu {
+
+    private String menuID;
+    private String menuName;
+    private Boolean menuPrice;
+    private Image menuImage;
+
+
+
 }
