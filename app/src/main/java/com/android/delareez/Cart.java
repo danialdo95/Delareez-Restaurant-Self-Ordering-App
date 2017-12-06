a6140e7498531bf96153417b69ad8d2fdebae793
danialdo95 <danialdo95@gmail.com>
2017-12-06 16:53:35 +0800

    commit

--- app/src/main/java/com/android/delareez/Cart.java
+++ app/src/main/java/com/android/delareez/Cart.java
@@ -2,10 +2,36 @@ package com.android.delareez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.DA.delareez.OrderDA;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.model.delareez.Order;
 
 
 public class Cart extends AppCompatActivity {
+
+    public static boolean add= true;
+    private static final String TAG = "Cart";
+    private RecyclerView mOrderList;
+    private DatabaseReference mFirebaseRef;
+    private DatabaseReference mDatabaseMenu;
+    private DatabaseReference mDatabaseCust;
+    private LinearLayoutManager manager;
+    private TextView total;
+    private FirebaseRecyclerAdapter<Order, Cart.ViewHolder> firebaseRecyclerAdapter;
+    OrderDA Helper;
 
 
     @Override
@@ -13,13 +39,110 @@ public class Cart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
-
         if(getSupportActionBar()!=null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
+        mFirebaseRef = FirebaseDatabase.getInstance().getReference();
+        mDatabaseMenu = FirebaseDatabase.getInstance().getReference();
+        mDatabaseCust = FirebaseDatabase.getInstance().getReference();
+        total = (TextView) findViewById(R.id.total);
+
+
+        Query OrderQuery = mFirebaseRef.child("Order").orderByChild("cart").equalTo("in cart");
+        Helper=new OrderDA(mFirebaseRef);
+
+
+        mOrderList = (RecyclerView) findViewById(R.id.listCart);
+        manager = new LinearLayoutManager(this);
+        mOrderList.setHasFixedSize(true);
+
+
+
+
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Order, Cart.ViewHolder>(Order.class, R.layout.order_customer_card, Cart.ViewHolder.class, OrderQuery){
+            Double sum = 0.0;
+
+            @Override
+            protected void populateViewHolder(Cart.ViewHolder viewHolder, Order model, int position) {
+                final String post_key = getRef(position).getKey();
+                String menuid = model.getMenuID();
+                String custID = model.getCustID();
+
+
+                    mDatabaseMenu.child("Menu").child(menuid).child("menuName").addListenerForSingleValueEvent(new ValueEventListener() {
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String menuname = dataSnapshot.getValue(String.class);
+                            viewHolder.menuName.setText(menuname);
+                        }
+
+                        public void onCancelled(DatabaseError firebaseError) {
+                        }
+                    });
+
+                    mDatabaseCust.child("Customer").child(custID).child("custEmail").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String Cust = dataSnapshot.getValue(String.class);
+                            viewHolder.email.setText(Cust);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+                    sum = sum + model.getTotalPaymentPrice();
+                    total.setText(sum.toString());
+
+
+                    viewHolder.mQuantity.setText(Integer.toString(model.getNumberOfMenu()));
+                    viewHolder.Orderstatus.setText(model.getOrderStatus());
+                }
+
+
+
+
+        };
+
+
+        mOrderList.setAdapter(firebaseRecyclerAdapter);
+        mOrderList.setLayoutManager(manager);
+
+
     }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView menuName;
+        public final TextView mQuantity;
+        public final TextView tablenum;
+        public final TextView Date;
+        public final TextView Orderstatus;
+        public final TextView email;
+
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            menuName = (TextView) itemView.findViewById(R.id.card_title);
+            mQuantity = (TextView) itemView.findViewById(R.id.drink_price);
+            tablenum = (TextView) itemView.findViewById(R.id.textView9);
+            Date = (TextView) itemView.findViewById(R.id.textView8);
+            Orderstatus = (TextView) itemView.findViewById(R.id.textView5);
+            email = (TextView) itemView.findViewById(R.id.textView4);
+
+        }
+
+    }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
--- app/src/main/java/com/android/delareez/DrinkContent.java
+++ app/src/main/java/com/android/delareez/DrinkContent.java
@@ -1,22 +1,115 @@
 package com.android.delareez;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.model.delareez.Menu;
+import com.squareup.picasso.Picasso;
 
 public class DrinkContent extends Fragment {
 
+
+    private static final String TAG = "DrinkContent";
+    private RecyclerView mFoodList;
+    private DatabaseReference mFirebaseRef;
+    private LinearLayoutManager manager;
+    private FirebaseRecyclerAdapter<Menu, DrinkContent.ViewHolder> firebaseRecyclerAdapter;
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.activity_drink_content, container,false);
 
+        progressBar = (ProgressBar) myView.findViewById(R.id.LoadingFood);
+
+        mFirebaseRef = FirebaseDatabase.getInstance().getReference();
+        Query DrinkQuery = mFirebaseRef.child("Menu").orderByChild("menuType").equalTo("Drink");
+
+
+        mFoodList = (RecyclerView) myView.findViewById(R.id.food_list2);
+        manager = new LinearLayoutManager(this.getContext());
+        mFoodList.setHasFixedSize(true);
+        progressBar.setVisibility(View.VISIBLE);
+
+
+        //Initializes Recycler View and Layout Manager.
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Menu, DrinkContent.ViewHolder>(Menu.class, R.layout.food_card, DrinkContent.ViewHolder.class, DrinkQuery) {
+            @Override
+            protected void populateViewHolder(DrinkContent.ViewHolder viewHolder, Menu model, int position) {
+
+                final String post_key = getRef(position).getKey();
+                viewHolder.menuName.setText(model.getMenuName());
+                viewHolder.menuPrice.setText("RM " +Double.toString(model.getMenuPrice()) + "0");
+                viewHolder.menuStatus.setText(model.getMenuStatus());
+                if (model.getMenuStatus().equals("Available")){
+                    viewHolder.menuStatus.setTextColor(Color.GREEN);
+                }
+                else if (model.getMenuStatus().equals("Out Of Order")){
+                    viewHolder.menuStatus.setTextColor(Color.RED);
+                }
+                Picasso.with(viewHolder.menuImage.getContext()).load(model.getMenuImage()).into(viewHolder.menuImage);
+                viewHolder.OrderButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent orderFood = new Intent(getActivity(),OrderActivity.class);
+                        orderFood.putExtra("FoodID", post_key);
+
+                        startActivity(orderFood);
+                    }
+                });
+
+                progressBar.setVisibility(View.GONE);
+            }
+        };
+
+        mFoodList.setAdapter(firebaseRecyclerAdapter);
+        mFoodList.setLayoutManager(manager);
+
 
 
         return  myView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView menuName;
+        public final TextView menuPrice;
+        public final TextView menuStatus;
+        public final ImageView menuImage;
+        public final Button OrderButton;
+
+
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            menuName = (TextView) itemView.findViewById(R.id.card_title);
+            menuPrice = (TextView) itemView.findViewById(R.id.drink_price);
+            menuStatus = (TextView) itemView.findViewById(R.id.Status);
+            menuImage = (ImageView) itemView.findViewById(R.id.drink_image);
+            OrderButton = (Button) itemView.findViewById(R.id.Order);
+
+        }
+
+
+
     }
 }
